fb15a9996c508de740fcdcc9af2ff8fad9e94890
Thomas Sanders <thomas.sanders@citrix.com>
2015-06-05 09:59:29 +0100

    CP-12511 Temp commit: use SSL back-compat mode
    Temporary code-change to always allow old-style SSLv3 incoming
    connections as in XenServer 6.5 and earlier.
    
    This is needed for the time being even with latest development versions
    of XenServer.  Once XenServer has a newer stunnel that can use TLSv1.2
    (protocol and ciphersuites) for outgoing connections, we can undo this
    commit without breaking inter-host communications.
    
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>
    (cherry picked from commit 7f0fee9dd54057792a7054e45306a38fe962cc43)
    
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>

--- ocaml/xapi/xapi_mgmt_iface.ml
+++ ocaml/xapi/xapi_mgmt_iface.ml
@@ -42,7 +42,9 @@ let restart_stunnel ~__context =
 	let xapissl_args = [ "restart" ] @ 
 		if Helpers.rolling_upgrade_in_progress ~__context
 		then [ "back_compat_6_5" ]
-		else []
+(*		else [] *)
+(*		TODO use line above when trunk no longer needs back-compat mode *)
+		else [ "back_compat_6_5" ]
 	in
 	let (_ : Thread.t) = Thread.create (fun () ->
 		Mutex.execute management_m (fun () ->
