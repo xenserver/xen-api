b24bed795c79ae0a5c8fb9c640eb27818e283e08
John Else <john.else@citrix.com>
2015-09-02 14:30:01 +0000

    CA-180015: Handle copies in Storage_migrate.stat
    Make sure Storage_migrate.stat looks in the new active_copy table, as
    well as active_recv and active_send.
    
    Signed-off-by: John Else <john.else@citrix.com>
    (cherry picked from commit d59a5c7fde5d480bfe96cbde0cd5ef367cad8b16)
    
    Conflicts:
    	ocaml/xapi/storage_migrate.ml

--- ocaml/xapi/storage_migrate.ml
+++ ocaml/xapi/storage_migrate.ml
@@ -494,6 +494,7 @@ let stop ~dbg ~id =
 let stat ~dbg ~id =
 	let recv_opt = State.find id State.active_recv in
 	let send_opt = State.find id State.active_send in
+	let copy_opt = State.find id State.active_copy in
 	let open State in
 	let failed = match send_opt with
 		| Some (Send s) ->
@@ -510,15 +511,20 @@ let stat ~dbg ~id =
 		| _ -> false
 	in
 	let open Mirror in
-	let state = match (recv_opt,send_opt) with
-		| (Some _, Some _) -> [Sending; Receiving] 
-		| (Some _, None) -> [Receiving]
-		| (None, Some _) -> [Sending]
-		| (None, None) -> raise (Does_not_exist ("mirror",id))
+	let state =
+		(match recv_opt with Some _ -> [Receiving] | None -> []) @
+		(match send_opt with Some _ -> [Sending]   | None -> []) @
+		(match copy_opt with Some _ -> [Copying]   | None -> [])
 	in
-	let (sr,vdi) = of_mirror_id id in
-	let src = match (recv_opt,send_opt) with | (Some (Receive x), _) -> x.Receive_state.remote_vdi | (_,Some (Send x)) -> vdi | _ -> failwith "Invalid" in
-	let dst = match (recv_opt,send_opt) with | (Some (Receive x), _) -> x.Receive_state.leaf_vdi | (_,Some (Send x)) -> x.Send_state.mirror_vdi | _ -> failwith "Invalid" in
+	if state = [] then raise (Does_not_exist ("mirror", id));
+	let src, dst = match (recv_opt, send_opt, copy_opt) with
+	| (Some (Receive receive_state), _, _) ->
+		receive_state.Receive_state.remote_vdi, receive_state.Receive_state.leaf_vdi
+	| (_, Some (Send send_state), _) ->
+		snd (of_mirror_id id), send_state.Send_state.mirror_vdi
+	| (_, _, Some (Copy copy_state)) ->
+		snd (of_copy_id id), copy_state.Copy_state.copy_vdi
+	| _ -> failwith "Invalid" in
 	{ Mirror.source_vdi = src; dest_vdi = dst; state; failed; }
 
 let list ~dbg =
