834209904f4db81037557854dec62e96b8a6f422
John Else <john.else@citrix.com>
2016-06-10 08:54:48 +0100

    Use corosync as default cluster stack
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -811,7 +811,7 @@ let sm_dir = ref "/opt/xensource/sm"
 let web_dir = ref "/opt/xensource/www"
 
 let cluster_stack_root = ref "/usr/libexec/xapi/cluster-stack"
-let cluster_stack_default = ref "xhad"
+let cluster_stack_default = ref "corosync"
 
 let xen_cmdline_path = ref "/opt/xensource/libexec/xen-cmdline"
 
