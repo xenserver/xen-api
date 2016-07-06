d968ac7dee12154e1f6fd483623afed6da326a03
John Else <john.else@citrix.com>
2016-07-06 19:26:12 +0100

    CP-17864: Add tests for Xapi_pvs_cache
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/test/suite.ml
+++ ocaml/test/suite.ml
@@ -51,6 +51,7 @@ let base_suite =
 			(* Test_ca121350.test; *)
 			Test_daily_license_check.test;
 			Test_dbsync_master.test;
+			Test_pvs_cache.test;
 			Test_pvs_farm.test;
 			Test_pvs_proxy.test;
 			Test_pvs_server.test;
