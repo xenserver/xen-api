507b3ddccecbb89dcd45aaebc34ac0d294c5a4c6
Rob Hoes <rob.hoes@citrix.com>
2016-07-07 23:43:24 +0100

    Add find_cache_vdi function
    Signed-off-by: Rob Hoes <rob.hoes@citrix.com>

--- ocaml/xapi/xapi_pvs_cache.ml
+++ ocaml/xapi/xapi_pvs_cache.ml
@@ -17,6 +17,7 @@ open Listext
 open Threadext
 
 exception No_cache_sr_available
+exception No_cache_vdi_present
 
 module VDI = struct
 	(* TODO: make this configurable. *)
@@ -80,6 +81,11 @@ let find_or_create_cache_vdi ~__context ~host ~farm =
 		| None -> raise No_cache_sr_available
 		| Some (sr, None) -> sr, VDI.create ~__context ~sr
 		| Some (sr, Some vdi) -> sr, vdi)
+
+let find_cache_vdi ~__context ~sr =
+	match VDI.find_one ~__context ~sr with
+	| None -> raise No_cache_vdi_present
+	| Some vdi -> vdi
 
 let on_sr_remove ~__context ~sr =
 	match VDI.find_all ~__context ~sr with
