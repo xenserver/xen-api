3c3aeaaec16d1afceb3c11d6284d2910b88d1756
John Else <john.else@citrix.com>
2016-07-14 15:20:02 +0100

    CP-17652: Add PVS_proxy feature flag
    Signed-off-by: John Else <john.else@citrix.com>

--- ocaml/xapi/features.ml
+++ ocaml/xapi/features.ml
@@ -50,6 +50,7 @@ type feature =
 	| Guest_ip_setting
 	| AD
 	| Ssl_legacy_switch
+	| PVS_proxy
 	with rpc
 
 type orientation = Positive | Negative
@@ -89,6 +90,7 @@ let keys_of_features =
 		Guest_ip_setting, ("restrict_guest_ip_setting", Negative, "GuestIP");
 		AD, ("restrict_ad", Negative, "AD");
 		Ssl_legacy_switch, ("restrict_ssl_legacy_switch", Negative, "Ssl_legacy_switch");
+		PVS_proxy, ("restrict_pvs_proxy", Negative, "PVS_proxy");
 	]
 
 let name_of_feature f =
