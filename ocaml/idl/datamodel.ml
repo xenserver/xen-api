62b3a9887ad0080c4e9346be4b71cb29fcb73e1a
Cheng Zhang <cheng.zhang@citrix.com>
2016-08-29 05:21:55 +0000

    CP-18254: Expose mounted updates over HTTP
    Signed-off-by: Cheng Zhang <cheng.zhang@citrix.com>

--- ocaml/idl/datamodel.ml
+++ ocaml/idl/datamodel.ml
@@ -9164,6 +9164,7 @@ let http_actions = [
   ("post_json_options", (Options, Constants.json_uri, false, [], _R_READ_ONLY, []));
   ("post_jsonrpc", (Post, Constants.jsonrpc_uri, false, [], _R_READ_ONLY, []));
   ("post_jsonrpc_options", (Options, Constants.jsonrpc_uri, false, [], _R_READ_ONLY, []));
+  ("get_pool_update_download", (Get, Constants.get_pool_update_download_uri, false, [], _R_READ_ONLY, []));
 ]
 
 (* these public http actions will NOT be checked by RBAC *)
@@ -9181,6 +9182,7 @@ let public_http_actions_with_no_rbac_check =
 		"post_json_options"; 
 		"post_jsonrpc";
 		"post_jsonrpc_options";
+		"get_pool_update_download";
 	]
 
 (* permissions not associated with any object message or field *)
