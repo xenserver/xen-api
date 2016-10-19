0316725a2fc1941f120b4c22bb3d0fa4a105f617
Thomas Sanders <thomas.sanders@citrix.com>
2016-10-19 15:56:06 +0100

    CA-220466: Guest reads/deletes in dom0 via grub config
    Bug: a guest can write a grub config that will cause an arbitrary dom0
    file to be made available to the guest as a ramdisk, and then deleted.
    
    Fix: use output-format=simple for pygrub (instead of the sexp-based
    output format).
    
    This change also expects the same line-based format from eliloader, so
    it depends on a corresponding change to eliloader, which needs to emit
    its output in the line-based "simple" format.
    
    Signed-off-by: Thomas Sanders <thomas.sanders@citrix.com>
    (cherry picked from commit 65284691bcb82cf55757444bb75cd3cc88a56f8d)
    The original commit was in the xenopsd repository rather than xen-api.
    
    Conflicts:
    
    	lib/bootloader.ml
    (cherry picked from commit 54cbed1dcf578b2cd48405df098024ea84f7be25)
    
    Conflicts:
    
    	ocaml/xenops/bootloader.ml
    (cherry picked from commit e90bb42a3282f5732cac608fd1cf63af6cf72d3b)

--- ocaml/xenops/bootloader.ml
+++ ocaml/xenops/bootloader.ml
@@ -63,23 +63,60 @@ let bootloader_args q extra_args legacy_args pv_bootloader_args image vm_uuid =
   ] @ pv_bootloader_args @ [
     image ]
 
-let parse_output x = 
-  let sexpr = "(" ^ x ^ ")" in
-  let sexpr' = SExpr_TS.of_string sexpr in
-  match sexpr' with
-    (* linux (kernel /var/lib/xen/vmlinuz.y1Wmrp)(args 'root=/dev/sda1 ro') *)
-    (* linux (kernel /var/lib/xen/vmlinuz.SFO5fb)(ramdisk /var/lib/xen/initrd.MUitgP)(args 'root=/dev/sda1 ro') *)
-    | SExpr.Node (SExpr.Symbol "linux" :: list) ->
-	let l = List.map (function
-	   | SExpr.Node [ SExpr.Symbol x; SExpr.Symbol y | SExpr.String y ] -> (x,y)
-	   | _ -> raise (Bad_sexpr sexpr)) list in
-	{
-		kernel_path = List.assoc "kernel" l;
-		initrd_path = (try Some (List.assoc "ramdisk" l) with _ -> None);
-		kernel_args = (try List.assoc "args" l with _ -> "") }
-    | _ -> 
-		debug "Failed to parse: %s" sexpr;
-		raise (Bad_sexpr sexpr)
+(* The string to parse comes from eliloader or pygrub, which builds it based on
+ * reading and processing the grub configuration from the guest's disc.
+ * Therefore it may contain malicious content from the guest if pygrub has not
+ * cleaned it up sufficiently. *)
+(* Example of a valid three-line string to parse, with blank third line:
+ * kernel <kernel:/vmlinuz-2.6.18-412.el5xen>
+ * args ro root=/dev/VolGroup00/LogVol00 console=ttyS0,115200n8
+ *
+ *)
+type acc_t = {kernel: string option; ramdisk: string option; args: string option}
+let parse_output_simple x =
+  let parse_line_optimistic acc l =
+    (* String.index will raise Not_found on the empty line that pygrub includes
+     * at the end of its simple-format output. *)
+    let space_pos = String.index l ' ' in
+    let first_word = String.sub l 0 space_pos in
+    let pos = space_pos + 1 in
+	match first_word with
+      | "kernel" -> (
+        match acc.kernel with
+          | Some _ -> raise (Bad_error ("More than one kernel line when parsing bootloader result: "^x))
+          | None ->
+            debug "Using kernel line from bootloader output: %s" l;
+            {acc with kernel = Some (String.sub l pos (String.length l - pos))} )
+      | "ramdisk" -> (
+        match acc.ramdisk with
+          | Some _ -> raise (Bad_error ("More than one ramdisk line when parsing bootloader result: "^x))
+          | None ->
+            debug "Using ramdisk line from bootloader output: %s" l;
+            {acc with ramdisk = Some (String.sub l pos (String.length l - pos))} )
+      | "args" -> (
+        match acc.args with
+          | Some _ -> raise (Bad_error ("More than one args line when parsing bootloader result: "^x))
+          | None ->
+            debug "Using args line from bootloader output: %s" l;
+            {acc with args = Some (String.sub l pos (String.length l - pos))} )
+      | "" -> acc
+      | _ -> raise (Bad_error ("Unrecognised start of line when parsing bootloader result: line="^l))
+  in
+  let parse_line acc l =
+    try parse_line_optimistic acc l
+    with Not_found -> acc
+  in
+  let linelist = Stdext.Xstringext.String.split '\n' x in
+  let content = List.fold_left parse_line {kernel=None; ramdisk=None; args=None} linelist in
+  {
+    kernel_path = (match content.kernel with
+      | None -> raise (Bad_error ("No kernel found in "^x))
+      | Some p -> p);
+    initrd_path = content.ramdisk;
+    kernel_args = (match content.args with
+      | None -> ""
+      | Some a -> a)
+  }
 
 let parse_exception x =
 	debug "Bootloader failed: %s" x;
@@ -104,7 +141,8 @@ let extract (task: Xenops_task.t) ~bootloader ~disk ?(legacy_args="") ?(extra_ar
 	debug "Bootloader commandline: %s %s\n" bootloader_path (String.concat " " cmdline);
 	try
 		let output, _ = Cancel_utils.cancellable_subprocess task bootloader_path cmdline in
-		parse_output output
+		debug "Bootloader output: %s" output;
+		parse_output_simple output
 	with Forkhelpers.Spawn_internal_error(stderr, stdout, _) ->
 		parse_exception stderr
 
